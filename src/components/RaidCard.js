d1e4fe5507e026d76be085f75e4b67dc4b9009cc
Rasmus Laine <rasmus.laine@gmail.com>
2018-09-05 23:41:17 +0300

    raid cards are more dynamic and accepts raid prop

--- src/components/RaidCard.js
+++ src/components/RaidCard.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import raidImage from '../images/raid.jpg'; 
+import RaidList from './RaidList';
 
-const card = () => {
+const card = (props) => {
+
+  if(!props.raid){
+    return (
+      <li>wrong raid info</li>
+    )
+  } 
+  const { level, gym: { name: gymName }}  = props.raid;
+
+
   return (
     <li>
       <div className="raid-strength">
@@ -12,8 +22,9 @@ const card = () => {
         <div className="raid-timers"> 30 minutes </div>
       </div>
       <div className="raid-info">
-        <p className="raid-gym-name">Betoniporsaat </p>
+        <p className="raid-gym-name">{ gymName || "betoniporsaat"} </p>
         <p className="raid-people-waiting">There is 3 people waiting </p>
+        <p className="raid-people-rating">{level} </p>
       </div>
       <button className="gym-btn-more-info">More info</button>
      
--- src/components/RaidList.js
+++ src/components/RaidList.js
@@ -6,19 +6,54 @@ import RaidCard from './RaidCard';
 class RaidList extends Component {
 
   raidList(){
+    const raids = [
+      { 
+        boss:'machamp',
+        endTime: 900, //s
+        level: 4,
+        playerQue: 2,
+        gym: {
+          name: 'betoniporsaat',
+          location: {
+            lat: '23424.23423',
+            lon: '1432342.324'
+          }
+        }
+      },
+      { 
+        boss:'lugia',
+        endTime: 5400, //s
+        level: 5,
+        playerQue: 0,
+        gym: {
+          name: 'kukkeli',
+          location: {
+            lat: '23424.23423',
+            lon: '1432342.324'
+          }
+        }
+      },
+    ];
     
+
+    return (
+      <ul>
+        {
+          raids.map(function(raid){
+            return <RaidCard raid={raid}/>
+          })
+        }
+      </ul>
+    )
+
   }
+
 
   render() {
     return (
       <div className="raid-container">
         <h2>Raid in progress</h2>
-        <ul>
-          <RaidCard />
-          <RaidCard />
-          <RaidCard />
-        </ul>
-
+          {this.raidList()}
         <h2>incoming raids</h2>
         <ul>
           <RaidCard />
