f3c52c375416a7c77a6208ce485e2c325c3d1833
Rasmus Laine <rasmus.laine@gmail.com>
2018-09-07 12:13:22 +0300

    added timer

--- src/components/RaidList.js
+++ src/components/RaidList.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react';
 
 import RaidCard from './RaidCard';
-
+import { Timer } from './Timer';
 
 class RaidList extends Component {
 
   raidList(){
+
+    const gyms = [
+      {
+        id:1,
+        name: 'betoniporsaat',
+        location: {
+          lat: '23424.23423',
+          lon: '1432342.324'
+        }
+      },
+      {
+        id:2,
+        name: 'kukkeli',
+        location: {
+          lat: '23424.23423',
+          lon: '1432342.324'
+        }
+      }
+    ];
+
     const raids = [
       { 
+        id: 1,
         boss:'machamp',
         endTime: 900, //s
         level: 4,
         playerQue: 2,
-        gym: {
-          name: 'betoniporsaat',
-          location: {
-            lat: '23424.23423',
-            lon: '1432342.324'
-          }
-        }
+        gym: 1
       },
       { 
+        id: 2,
         boss:'lugia',
         endTime: 5400, //s
         level: 5,
         playerQue: 0,
-        gym: {
-          name: 'kukkeli',
-          location: {
-            lat: '23424.23423',
-            lon: '1432342.324'
-          }
-        }
+        gym: 2
       },
     ];
+
+    
+    const gymRaids = raids.map(function(raid){
+      
+      const gym = gyms.filter(gym => gym.id === raid.gym);
+      raid.gym = gym[0];
+      return raid; 
+    })
+    console.log(gymRaids);
     
 
     return (
       <ul>
         {
-          raids.map(function(raid){
-            return <RaidCard raid={raid}/>
+          gymRaids.map(function(gymRaid){
+            return <RaidCard raid={gymRaid}/>
           })
         }
       </ul>
@@ -48,6 +67,13 @@ class RaidList extends Component {
 
   }
 
+  onEveryMinuteUpdateTimeElapsed() {
+    console.log('minut');
+  }
+
+  onCompletion() {
+    console.log('oncomplete');
+  }
 
   render() {
     return (
@@ -56,9 +82,10 @@ class RaidList extends Component {
           {this.raidList()}
         <h2>incoming raids</h2>
         <ul>
-          <RaidCard />
-          <RaidCard />
-          <RaidCard />
+          <Timer timeRemainingInSeconds={200}
+          onEveryMinute={() => this.onEveryMinuteUpdateTimeElapsed() }
+          onCompletion={() => this.onCompletion()}/>
+
         </ul>
       </div>
     );
---
+++ src/components/Timer.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+
+export class Timer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      remainingMinutes: 0,
+      remainingSeconds: 0
+    };
+  }
+
+  updateRemainMinutesAndSeconds(timeRemainingInSeconds){
+    let remainingMinutes = Math.floor(timeRemainingInSeconds/60);
+    let remainingSeconds = timeRemainingInSeconds % 60;
+    this.setState({
+      remainingMinutes,
+      remainingSeconds
+    });
+  }
+
+  countDown(timeRemainingInSeconds,shouldSkipCallback){
+    this.setState({
+      timeRemainingInSeconds
+    });
+    if (!shouldSkipCallback && timeRemainingInSeconds % 60 === 0) {
+      this.props.onEveryMinute(1);
+    }
+    if (timeRemainingInSeconds === 0){
+      this.props.onCompletion();
+    }
+    localStorage.setItem('timeRemainingInSeconds',timeRemainingInSeconds);
+    if(timeRemainingInSeconds > 0){
+      this.updateRemainMinutesAndSeconds(timeRemainingInSeconds);
+      timeRemainingInSeconds = timeRemainingInSeconds-1;
+      this.setTimeoutId = setTimeout(this.countDown.bind(this,timeRemainingInSeconds, false), 1000);
+    }
+  }
+
+  compareServerTimeAndComponentTimeAndUpdateServer(serverSideTimeRemainingInSeconds){
+    let componentTimeRemainingInSeconds = localStorage.getItem('timeRemainingInSeconds');
+    if(componentTimeRemainingInSeconds && componentTimeRemainingInSeconds < serverSideTimeRemainingInSeconds) {
+      let differenceInMinutes = Math.floor((serverSideTimeRemainingInSeconds - componentTimeRemainingInSeconds)/60)
+      if(differenceInMinutes>0){
+        this.props.onEveryMinute(differenceInMinutes)
+      }
+      return componentTimeRemainingInSeconds;
+    }
+    return serverSideTimeRemainingInSeconds;
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(this.props.timeRemainingInSeconds !== nextProps.timeRemainingInSeconds){
+      let timeRemainingInSeconds = this.compareServerTimeAndComponentTimeAndUpdateServer(nextProps.timeRemainingInSeconds);
+      this.countDown(timeRemainingInSeconds,true);
+    }
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.setTimeoutId);
+  }
+
+  render(){
+    return (
+      <div className='timer'>
+          <div>  
+            <div className='font-weight-bold lead number-display'>
+              {
+                this.state.remainingMinutes>9?
+                this.state.remainingMinutes:'0'+this.state.remainingMinutes
+              }:{
+                this.state.remainingSeconds>9?
+                this.state.remainingSeconds:'0'+this.state.remainingSeconds
+              }
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
