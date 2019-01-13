a4fdc79e43d9a2ea1f24cf9f2a7f93bd0eec11fb
Rasmus Laine <rasmus.laine@gmail.com>
2019-01-13 21:44:03 +0200

    added prod env

---
+++ src/config/prod.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const FirebaseConfig = {
+  apiKey: process.env.FIREBASE_API_kEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_PROJECT_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID
+};
